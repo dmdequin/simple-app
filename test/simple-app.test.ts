987468ec854da007ab70343b0aedcff8ee2b6bab
Danielle Dequin <dmdequin@gmail.com>
2023-12-08 21:58:50 +0100

    adding resource tests

--- test/simple-app.test.ts
+++ test/simple-app.test.ts
@@ -1,17 +1,38 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as SimpleApp from '../lib/simple-app-stack';
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import * as SimpleApp from '../lib/simple-app-stack';
+// import 'aws-cdk-lib/assert/jest';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/simple-app-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new SimpleApp.SimpleAppStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+test('Simple App Stack', () => {
+    const app = new cdk.App();
+    // WHEN
+    const stack = new SimpleApp.SimpleAppStack(app, 'MyTestStack');
+    // THEN
+    const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+     template.templateMatches({
+        Resources: {
+            "MySimpleAppBucket6B59014A": {
+            "Type": "AWS::S3::Bucket",
+            "Properties": {
+                "BucketEncryption": {
+                "ServerSideEncryptionConfiguration": [
+                {
+                "ServerSideEncryptionByDefault": {
+                    "SSEAlgorithm": "AES256"
+                }
+                }
+                ]
+                },
+                "VersioningConfiguration": {
+                "Status": "Enabled"
+                }
+            },
+            "UpdateReplacePolicy": "Retain",
+            "DeletionPolicy": "Retain",
+            },
+        },
+      });
+
+   template.hasResourceProperties('AWS::S3::Bucket', {});
 });
