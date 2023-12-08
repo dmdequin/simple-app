1133fe5cae1f5505a1e0ae71f5089ba9e292a19d
Danielle Dequin <dmdequin@gmail.com>
2023-12-08 22:52:31 +0100

    used eslint to make code formatting changes

--- lib/simple-app-stack.ts
+++ lib/simple-app-stack.ts
@@ -1,22 +1,22 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import { aws_s3 as s3 } from 'aws-cdk-lib';
+import * as cdk from 'aws-cdk-lib'
+import { type Construct } from 'constructs'
+import { aws_s3 as s3 } from 'aws-cdk-lib'
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class SimpleAppStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+  constructor (scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props)
 
     // resource
     const bucket = new s3.Bucket(this, 'MySimpleAppBucket', {
       versioned: true,
-      encryption: s3.BucketEncryption.S3_MANAGED,
-    });
+      encryption: s3.BucketEncryption.S3_MANAGED
+    })
 
+    // eslint-disable-next-line no-new
     new cdk.CfnOutput(this, 'MySimpleAppBucketNameExport', {
       value: bucket.bucketName,
-      exportName: 'MySimpleAppBucketName',
-    });
-
+      exportName: 'MySimpleAppBucketName'
+    })
   }
 }
--- test/simple-app.test.ts
+++ test/simple-app.test.ts
@@ -1,48 +1,48 @@
-import * as cdk from 'aws-cdk-lib';
-import { Template } from 'aws-cdk-lib/assertions';
-import * as SimpleApp from '../lib/simple-app-stack';
+import * as cdk from 'aws-cdk-lib'
+import { Template } from 'aws-cdk-lib/assertions'
+import * as SimpleApp from '../lib/simple-app-stack'
 // import 'aws-cdk-lib/assert/jest';
 
 test('Simple App Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new SimpleApp.SimpleAppStack(app, 'MyTestStack');
-    // THEN
-    const template = Template.fromStack(stack);
+  const app = new cdk.App()
+  // WHEN
+  const stack = new SimpleApp.SimpleAppStack(app, 'MyTestStack')
+  // THEN
+  const template = Template.fromStack(stack)
 
-    template.templateMatches({
-        Resources: {
-            "MySimpleAppBucket6B59014A": {
-                "Type": "AWS::S3::Bucket",
-                "Properties": {
-                    "BucketEncryption": {
-                        "ServerSideEncryptionConfiguration": [
-                            {
-                                "ServerSideEncryptionByDefault": {
-                                    "SSEAlgorithm": "AES256"
-                                }
-                            }
-                        ]
-                    },
-                    "VersioningConfiguration": {
-                    "Status": "Enabled"
-                    }
-                },
-                "UpdateReplacePolicy": "Retain",
-                "DeletionPolicy": "Retain",
-            },
-        },
-        "Outputs": {
-            "MySimpleAppBucketNameExport": {
-                "Value": {
-                    "Ref": "MySimpleAppBucket6B59014A"
-                },
-                "Export": {
-                    "Name": "MySimpleAppBucketName"
+  template.templateMatches({
+    Resources: {
+      MySimpleAppBucket6B59014A: {
+        Type: 'AWS::S3::Bucket',
+        Properties: {
+          BucketEncryption: {
+            ServerSideEncryptionConfiguration: [
+              {
+                ServerSideEncryptionByDefault: {
+                  SSEAlgorithm: 'AES256'
                 }
-            }
+              }
+            ]
+          },
+          VersioningConfiguration: {
+            Status: 'Enabled'
+          }
         },
-    });
+        UpdateReplacePolicy: 'Retain',
+        DeletionPolicy: 'Retain'
+      }
+    },
+    Outputs: {
+      MySimpleAppBucketNameExport: {
+        Value: {
+          Ref: 'MySimpleAppBucket6B59014A'
+        },
+        Export: {
+          Name: 'MySimpleAppBucketName'
+        }
+      }
+    }
+  })
 
-    template.hasResourceProperties('AWS::S3::Bucket', {});
-});
+  template.hasResourceProperties('AWS::S3::Bucket', {})
+})
