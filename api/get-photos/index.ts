cae65d0d1acd91e55fe4183aed6ec030b4bba9d9
Danielle Dequin <dmdequin@gmail.com>
2023-12-13 15:58:40 +0100

    Link s3 bucket to lambda function

--- api/get-photos/index.ts
+++ api/get-photos/index.ts
@@ -1,20 +1,32 @@
-// lambda home
 import {
   type APIGatewayProxyEventV2,
   type Context,
   type APIGatewayProxyResultV2
 } from 'aws-lambda'
+import { S3 } from 'aws-sdk'
 
-const bucketName = process.env.PHOTO_BUCKET_NAME
+const bucketName = process.env.PHOTO_BUCKET_NAME!
+const s3 = new S3()
+
+function generateUrl(asset){}
 
 async function getPhotos (
   event: APIGatewayProxyEventV2,
   context: Context):
   Promise<APIGatewayProxyResultV2> {
   console.log('I got the bucket name and it is ' + bucketName)
-  return {
-    statusCode: 200,
-    body: 'Hello from lambda. It is live!'
+  try{
+    const {Contents: results} = await s3.listObjects({Bucket: bucketName}).promise()
+    const photos = await Promise.all(results!.map(result => generateUrl(result)))
+    return {
+      statusCode: 200,
+      body: JSON.stringify(photos)
+    }
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: error.message
+    }
   }
 }
 
